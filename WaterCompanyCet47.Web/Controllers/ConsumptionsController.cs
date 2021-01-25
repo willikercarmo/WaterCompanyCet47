45cd5bf4685892ba27a6da1ebc04b3212e9c9320
willikercarmo <carmo.williker@gmail.com>
2021-01-25 08:37:47 +0000

    Create ConsumptionsController

---
+++ WaterCompanyCet47.Web/Controllers/ConsumptionsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WaterCompanyCet47.Web.Data.Repositories;
+
+namespace WaterCompanyCet47.Web.Controllers
+{
+    public class ConsumptionsController : Controller
+    {
+        private readonly IConsumptionRepository _consumptionRepository;
+
+        public ConsumptionsController(
+            IConsumptionRepository consumptionRepository)
+        {
+            _consumptionRepository = consumptionRepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var model = await _consumptionRepository.GetConsumptionAsync(this.User.Identity.Name);
+           
+            return View(model);
+        }
+    }
+}
